ef39f8482c64b4c8f3e14b7485d2b8aa64066ba4
vadym <vadym.shevchenko.programming@gmail.com>
2017-07-05 17:38:17 +0300

    Update 2017-07-05T14:38:15.658Z

--- javascripts.js
+++ javascripts.js
@@ -2,51 +2,63 @@ var dragElems = document.getElementsByClassName('drag-elem');
 console.log(dragElems);
 for (var i = 0; i < dragElems.length; i++) {
   dragElems[i].addEventListener('mousedown', (e) => {
-      var dragElem = e.target;
-      dragElem = filterDragElem(dragElem)
+    if (e.which != 1) { // right click
+      return;
+    }
+    var dragElem = e.target;
+    dragElem = filterDragElem(dragElem, 'drag-elem')
 
-      dragElem.ondragstart = function() {
-        return false;
-      };
+    dragElem.ondragstart = function() {
+      return false;
+    };
 
-      dragElem.classList.add('taken');
+    dragElem.classList.add('taken');
 
-      var coords = getCoords(dragElem);
-      var shiftX = e.pageX - coords.left;
-      var shiftY = e.pageY - coords.top;
-      moveAt(e)
-      dragElem.style.position = 'absolute';
-      // move function
-      function moveAt(e) {
-        dragElem.style.left = e.pageX - shiftX + 'px';
-        dragElem.style.top = e.pageY - shiftY + 'px';
+    var coords = getCoords(dragElem);
+    var shiftX = e.pageX - coords.left;
+    var shiftY = e.pageY - coords.top;
+    moveAt(e)
+    dragElem.style.position = 'absolute';
+    // move function
+    function moveAt(e) {
+      dragElem.style.left = e.pageX - shiftX + 'px';
+      dragElem.style.top = e.pageY - shiftY + 'px';
+    }
+
+    document.onmousemove = function(e) {
+      moveAt(e);
+      var area = searchPlace(e);
+      if( area.className.search('drop-area') >= 0 ) {
+        // do something anrial
       }
+    }
+    document.onmouseup = (e) => {
+      dragElem.classList.remove('taken');
+      dropArea = searchPlace(e);
+      elem = filterDragElem(e.target, 'drag-elem')
+      document.onmousemove = null;
+      dragElem.onmouseup = null;
 
-      document.onmousemove = function(e) {
-        moveAt(e);
-      }
-      document.onmouseup = (e) => {
-        dragElem.classList.remove('taken');
-        searchPlace(e);
-        document.onmousemove = null;
-        dragElem.onmouseup = null
-      }
-      function searchPlace (e) {
-        var elem = e.target;
-        elem = filterDragElem(elem)
-        console.log(elem)
-        elem.style.pointerEvents = 'none'
-        var dropArea = document.elementFromPoint(e.pageX, e.pageY);
-        elem.style.pointerEvents = 'auto'
+      if( dropArea.className.search('drop-area') >= 0 ) {
+        elem.style.position = 'relative';
+        elem.classList.add('fall');
+        dropArea.appendChild(elem );
+        elem.style.left = 0;
+        elem.style.top = 0;
+      } else {
+        setTimeout( ()=>{
+          var dragArea = document.getElementsByClassName('drag-area')[0];
+          dragArea.appendChild(elem)
+          elem.style.animation = 'hide .3s'
+          setTimeout( ()=>{
+            elem.style.position = 'relative';
+            elem.style.left = 0;
+            elem.style.top = 0;
+          }, 300)
 
-        // debugger
-        if( dropArea.className.search('drop-area') >= 0 ) {
-          elem.style.position = 'relative';
-          dropArea.appendChild(elem.parentNode);
-          elem.style.left = 0
-          elem.style.top = 0
-        }
+        }, 200)
       }
+    }
   })
 }
 
@@ -54,13 +66,24 @@ for (var i = 0; i < dragElems.length; i++) {
 function getCoords(elem) {
   return elem.getBoundingClientRect();
 }
+
 // filter elements
-function filterDragElem(dragElem) {
-  while( dragElem.className.search('drag-elem') < 0 ) {
+function filterDragElem(dragElem, searchClass) {
+  while( dragElem.className.search(searchClass) < 0 ) {
     dragElem = dragElem.parentNode
     if (dragElem.nodeName == "BODY") {
       return
     }
   }
-  return dragElem
+  return dragElem;
+}
+
+// search elements
+function searchPlace (e) {
+  var elem = e.target;
+  elem = filterDragElem(elem, 'drag-elem')
+  elem.style.pointerEvents = 'none';
+  var dropArea = document.elementFromPoint(e.pageX, e.pageY);
+  elem.style.pointerEvents = 'auto';
+  return dropArea;
 }
